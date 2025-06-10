a5c3aaf8e0055253199881108916c1e4095661aa
JazzoLopez <Jazzielrodriguezlopez@gmail.com>
2025-06-09 23:51:27 -0600

    feat(logging): add logging to application startup

--- src/main/java/com/sentinelpi/sentinelpi/SentinelpiApplication.java
+++ src/main/java/com/sentinelpi/sentinelpi/SentinelpiApplication.java
@@ -3,10 +3,13 @@ package com.sentinelpi.sentinelpi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
+@Slf4j
 public class SentinelpiApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting SentinelpiApplication...");
 		SpringApplication.run(SentinelpiApplication.class, args);
 	}
 
